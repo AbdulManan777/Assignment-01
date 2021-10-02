2132540a3d94df28f8452f4e408557bb74d437c7
Abdul Manan <i190500@nu.edu.pk>
2021-10-02 15:31:24 +0500

    Full Test cases files Test suite class

--- APAssign/src/test/java/Assignment01/AP/TestSuite.java
+++ APAssign/src/test/java/Assignment01/AP/TestSuite.java
@@ -7,12 +7,14 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
-@RunWith(Suite.class) /* 2 Test cases fail because of bugs in it that I have found and some 
-                       have exceptions also that I have explicitly mentioned else are pass*/
+@RunWith(Suite.class) 
 @SuiteClasses({ArrayTest.class, DequeueTest.class,CircularLinkedListTest.class, LinkedListTest.class,
                QueueArrayTest.class, QueueArray2Test.class, QueueLinkedListTest.class, StackArrayTest.class,
                StackLinkedListTest.class})
 public class TestSuite {
+	
+	/* 2 Test cases fail because of bugs in it that I have found and some 
+    have exceptions also that I have explicitly mentioned else are pass*/
 
 	
 
